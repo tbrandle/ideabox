10b8218f1c8c1aa7d6cf3920dca8e5c77e098332
Tim <tbrandle53@gmail.com>
2016-12-17 14:47:19 -0700

    Add object constructor Fn, add delete btn functionatlity, create new idea object on save btn

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+
+
+
+function Idea(titleInput, bodyInput){
+  this.title = titleInput
+  this.body = bodyInput
+  this.quality = "swill"  || quality //need to research and add switch case
+  // this.id = //need to research date.now function to call here
+}
+
+$('.idea-storage-list').on('click', '.delete-btn', function(){
+  console.log("hi");
+  $(this).parent().remove();
+}
+)
+
+
+
+
 $('.save-btn').on('click', function () {
   var titleInput = $('.title-user-input').val();
   var bodyInput = $('.body-user-input').val();
+  var idea = new Idea(titleInput, bodyInput);
+  console.log(idea);
   console.log(titleInput);
   $('.idea-storage-list').prepend(
     `<article class="idea">
