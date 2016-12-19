0208aee254f1c5b129c37ec40a04dc9f581e67c9
Tim <tbrandle53@gmail.com>
2016-12-19 15:43:50 -0700

    Experiement with restoring the local storage key/values

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-
+retrievedIdea();
+// var ideaArray = retrievedIdea().map(function() {
+//   return parsedIdea;
+// })
 
 
 function Idea(titleInput, bodyInput){
   this.title = titleInput
   this.body = bodyInput
   this.quality = "swill"  || quality //need to research and add switch case
-  // this.id = //need to research date.now function to call here
+  this.id = Date.now();
 }
 
-$('.idea-storage-list').on('click', '.delete-btn', function(){
-  console.log("hi");
-  $(this).parent().remove();
+function retrievedIdea() {
+  for(var i =0; i < localStorage.length; i++){
+    var retrievedObject = localStorage.getItem(localStorage.key(i));
+    var parsedIdea = JSON.parse(retrievedObject);
+    return parsedIdea;
+  }
 }
-)
-
-
-
 
 $('.save-btn').on('click', function () {
   var titleInput = $('.title-user-input').val();
   var bodyInput = $('.body-user-input').val();
   var idea = new Idea(titleInput, bodyInput);
+  var stringifiedIdea = JSON.stringify(idea);
+  localStorage.setItem(idea.id, stringifiedIdea);
   console.log(idea);
   console.log(titleInput);
   $('.idea-storage-list').prepend(
@@ -32,5 +36,17 @@ $('.save-btn').on('click', function () {
       <button class="down-vote-btn rating"type="button" name="button"></button>
       <p class="rating">Quality: swill</p>
     </article>`
-)
+  )
+
 })
+
+  // retrievedIdea = localStorage.getItem();
+  // var parsedIdea = JSON.parse(retrievedIdea)
+  // console.log(parsedIdea);
+// })
+
+
+$('.idea-storage-list').on('click', '.delete-btn', function(){
+  $(this).parent().remove();
+}
+)
