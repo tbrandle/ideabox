bb07b1b068718a7e4f68ad4b881f63121a036217
Tim <tbrandle53@gmail.com>
2016-12-20 09:05:14 -0700

    refactored prepend

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 var parsedArray = []
 
+function prepend(idea) {
+  $('.idea-storage-list').prepend(
+    `<article id="${idea.id}" class="idea">
+    <h2>${idea.title}</h2>
+    <input class="delete-btn" type="submit" value="" src="icons/delete.svg">
+    <p class="idea-description">${idea.body}</p>
+    <button class="up-vote-btn rating" type="button" name="button"></button>
+    <button class="down-vote-btn rating"type="button" name="button"></button>
+    <p class="rating">Quality: ${idea.quality}</p>
+    </article>`
+  );
+}
+
 $(document).ready(function() {
   for(var i =0; i < localStorage.length; i++){
     parsedArray.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
   }
   for(var i=0; i< parsedArray.length; i++){
-  $('.idea-storage-list').prepend(
-    `<article id="${parsedArray[i].id}" class="idea">
-      <h2>${parsedArray[i].title}</h2>
-      <input class="delete-btn" type="submit" value="" src="icons/delete.svg">
-      <p class="idea-description">${parsedArray[i].body}</p>
-      <button class="up-vote-btn rating" type="button" name="button"></button>
-      <button class="down-vote-btn rating"type="button" name="button"></button>
-      <p class="rating">Quality: ${parsedArray[i].quality}</p>
-    </article>`
-  )
+      prepend(parsedArray[i]);
 }
 })
+
 
 function Idea(titleInput, bodyInput){
   this.title = titleInput
@@ -29,16 +34,7 @@ $('.save-btn').on('click', function () {
   var idea = new Idea($('.title-user-input').val(), $('.body-user-input').val());
   var stringifiedIdea = JSON.stringify(idea);
   localStorage.setItem(idea.id, stringifiedIdea);
-  $('.idea-storage-list').prepend(
-    `<article id="${idea.id}" class="idea">
-      <h2>${idea.title}</h2>
-      <input class="delete-btn" type="submit" value="" src="icons/delete.svg">
-      <p class="idea-description">${idea.body}</p>
-      <button class="up-vote-btn rating" type="button" name="button"></button>
-      <button class="down-vote-btn rating"type="button" name="button"></button>
-      <p class="rating">Quality: ${idea.quality}</p>
-    </article>`
-  )
+  prepend(idea);
 })
 
 $('.idea-storage-list').on('click', '.delete-btn', function(){
